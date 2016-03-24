f37ea037bb5779cf15120c4cbfcc597c6f5b80ea
Beto23 <josee-45@hotmail.com>
2016-03-23 20:46:15 -0600

    se agrego modulo login con su diseño

---
+++ src/app/main/login/login.module.js
@@ -0,0 +1,4 @@
+module.exports = function (){
+  var login = angular.module('appBuenaSalud.login',[])
+  console.log('login');
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ require ('angular-ui-router');
 window.$ = window.jQuery = require('jquery');
 var app = angular.module('appBuenaSalud', [
   'ui.router',
-  'appBuenaSalud.home'
+  'appBuenaSalud.home',
+  'appBuenaSalud.login'
 ]);
 
 require('./app/home/home.module')();
+require('./app/main/login/login.module')();
 require('./app.config')(app);
