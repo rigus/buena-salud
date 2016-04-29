ab90a3a9b505db74def6820f0afe328fd9991e2b
Beto23 <josee-45@hotmail.com>
2016-04-29 15:54:06 -0500

    Add delete doctor

--- src/app/main/secretaria/doctores/listadod/doctor.controller.js
+++ src/app/main/secretaria/doctores/listadod/doctor.controller.js
@@ -1,10 +1,29 @@
 module.exports = function (ngModule){
   ngModule.controller('DoctorCtrl', DoctorCtrl)
   /*@ngInject*/
-  function DoctorCtrl (arrDoctores){
+  function DoctorCtrl (arrDoctores, DoctorService){
     console.log("doctorees");
     var vm = this;
     vm.doctores = arrDoctores;
+
+    vm.borrarDoctor = function(doctorSelected){
+      console.log('Borrando al doctor',doctorSelected)
+       DoctorService.deleteDoctor(doctorSelected)
+          .then(function(response){
+            console.log(response);
+            if (response.estatus == "ok") {
+                console.log('doctor eliminado');
+                //vm.isWarningOn = false;
+                $state.reload();
+                //body.append($compile("<alert-succes alertSucc='"+ vm.alertSucc  +"' vm.alertSucc='"+ true  +"'  ng-if='"+ vm.alertSucc  +"'  correcto='"+ response.msj +"'></alert-succes>")($scope));
+            }else {
+              console.log(response.msj);
+            }
+          }).catch(function(err){
+            console.log(err);
+          });
+    }
+
   }
 
 }
